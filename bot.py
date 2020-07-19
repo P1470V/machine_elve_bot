be0bb535794b6ecf2745555cda53487e67cff46d
PLATOV <nuitixp@gmail.com>
2020-07-19 19:20:27 +0200

    Added Help command

--- bot.py
+++ bot.py
@@ -1,16 +1,24 @@
 import discord
 from discord.ext import commands
 
-bot_token = input("Bot Token: ")
+help_file = open("help.txt", "r")
+bot_token = input("Bot Token: ") #Discord Bot Token
 
 client = commands.Bot(command_prefix="<")
 
+#Bot says that it's ready when it starts up
 @client.event
 async def on_ready():
     print("Bot is ready")
 
+#First Command, pretty self explanatory
 @client.command()
 async def hello(ctx):
     await ctx.send("Go Fuck yourself")
+
+#help command that will list all of the available commands
+@client.command()
+async def help(ctx):
+    await ctx.send(help_file.read())
 
 client.run(bot_token)
